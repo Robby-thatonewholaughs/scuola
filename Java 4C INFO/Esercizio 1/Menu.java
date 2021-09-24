f26369496b16438f46421e43fd157ad01aab6a05
Robby-thatonewholaughs <77329151+Robby-thatonewholaughs@users.noreply.github.com>
2021-09-24 16:10:02 +0200

    Update Menu.java

--- Java 4C INFO/Esercizio 1/Menu.java
+++ Java 4C INFO/Esercizio 1/Menu.java
@@ -14,39 +14,53 @@ public class Menu {
     int r=0;
     // Costruisco la struttura del menù creando anche un ciclo
     do{
-      // Visualizzo un messaggio di errore in caso il valore inserito non sia corretto
-      if ( r != 0 && r != 1 && r != 2){
-          System.out.println("Il valore inserito non e' corretto");
+      do{
+        // Visualizzo un messaggio di errore in caso il valore inserito non sia corretto
+        if ( r != 0 && r != 1 && r != 2){
+            System.out.print("\033[H\033[2J"); //Viene pulito lo schermo
+            System.out.flush();
+            System.out.println("Il valore inserito non e' corretto");
+            new Scanner(System.in).nextLine();
+            System.out.print("\033[H\033[2J"); //Viene pulito lo schermo
+            System.out.flush();
+        }
+        System.out.println("Esegui la tua selezione");
+        System.out.println("Seleziona un evento:");
+        System.out.println(" 0 - Termini il programma");
+        System.out.println(" 1 - Stampa Hello World");
+        System.out.println(" 2 - Mi presento");
+        System.out.print("Inserisci la tua scelta: ");
+        Scanner in = new Scanner(System.in);
+        // Acquisisco il valore per la scelta
+        r = in.nextInt();
+        //Verifico che il valore inserito dall'utente sia corretto
+      }while(r<0||r>2);
+      //Cancello cio' che c'e' scritto nella console
+      System.out.print("\033[H\033[2J");
+      System.out.flush();
+      //Definisco uno switch-case che esegue l'opzione selezionata dall'utente
+      switch(r){
+        case 0:
+        {
+          System.out.println("Programma terminato");
+          break;
+        }
+        case 1:
+        {
+          System.out.println("Hello World!");
+          System.out.println("Premere un tasto per continuare..." );
+          break;
+        }
+        case 2:
+        {
+          System.out.println("Il mio nome e cognome è : Christian Ruzza" );
+          System.out.println("Premere un tasto per continuare..." );
+          break;
+        }
       }
-      System.out.println("Esegui la tua selezione");
-      System.out.println("Seleziona un evento:");
-      System.out.println(" 0 - Termini il programma");
-      System.out.println(" 1 - Stampa Hello World");
-      System.out.println(" 2 - Mi presento");
-      System.out.print("Inserisci la tua scelta: ");
-      Scanner in = new Scanner(System.in);
-      // Acquisisco il valore per la scelta
-      r = in.nextInt();
-    }while(r<0||r>2);
-    //Verifico che il valore inserito dall'utente sia corretto
-
-    //Definisco uno switch-case che esegue l'opzione selezionata dall'utente
-    switch(r){
-      case 0:
-      {
-        System.out.println("Programma terminato");
-        break;
-      }
-      case 1:
-      {
-        System.out.println("Hello World");
-        break;
-      }
-      case 2:
-      {
-        System.out.println("Il mio nome e cognome è : Christian Ruzza" );
-        break;
-      }
-    }
+      new Scanner(System.in).nextLine(); //Viene messo in pausa il programma aspettando che l'utente prema un tasto
+      System.out.print("\033[H\033[2J");
+      System.out.flush();
+    }while(r!=0);
   }
 }
